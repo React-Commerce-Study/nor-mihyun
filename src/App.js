947e648fef98ff93f8a2546427a86cdf906d0cc4
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-05-23 00:54:55 +0900

    ✨feat: 메인 페이지 연결하기 (#2)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
