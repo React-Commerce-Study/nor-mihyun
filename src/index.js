bbc37869eafe33a7dda2009678bd2c2392ce9c7a
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-05-23 01:52:27 +0900

    💄design: CSS 초기화 및 글로벌 스타일 추가 (#2)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  mainColor: "#21BF48",
+  mainColorLight: "##EFFFF3",
+  darkGray: "#767676",
+  lightGray: "#C4C4C4",
+  red: "#EB5757",
+};
+
+export default theme;
