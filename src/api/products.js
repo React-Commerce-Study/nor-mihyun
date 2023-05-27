9919c8fe4a6da1258f33fde0f4fc18da68f37dec
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-05-27 21:35:15 +0900

    ✨feat: 상품 목록 api 커스텀 에러 추가 (#3)

--- src/api/products.js
+++ src/api/products.js
@@ -3,10 +3,14 @@ const URL = "https://openmarket.weniv.co.kr";
 export const getProductList = async () => {
   try {
     const response = await fetch(`${URL}/products`);
-    const data = await response.json();
 
+    if (!response.ok) {
+      throw new Error("네트워크에 문제가 발생했습니다.");
+    }
+
+    const data = await response.json();
     return data;
   } catch (error) {
-    console.error(error);
+    console.error("데이터를 가져올 수 없습니다:", error.message);
   }
 };
