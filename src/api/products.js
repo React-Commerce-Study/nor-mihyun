7e3bf0e78a724aeee68ed577346fbc4c58137049
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-04 02:40:54 +0900

    ✨feat: 상품 상세 정보 가져오는 api 추가 (#8)

--- src/api/products.js
+++ src/api/products.js
@@ -14,3 +14,18 @@ export const getProductList = async (page) => {
     console.error("데이터를 가져올 수 없습니다:", error.message);
   }
 };
+
+export const getProduct = async (productId) => {
+  try {
+    const response = await fetch(`${URL}/products/${productId}`);
+
+    if (!response.ok) {
+      throw new Error("네트워크에 문제가 발생했습니다.");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("데이터를 가져올 수 없습니다:", error.message);
+  }
+};
